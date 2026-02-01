f1a3968ab8661b95fc3f430fade228df3f821329
webgitnew <abdulkarim.raslan.7@gmail.com>
2026-02-01 16:52:40 +0530

    Delete git directory

--- git/js/script_support.js
+++
@@ -1,9 +0,0 @@
-<script>
-// فحص إذا تم تحميل CSS
-if (!document.querySelector('link[href*="w3.css"]')) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'css/w3.css';
-    document.head.appendChild(link);
-}
-</script>
